7a02e5cbdc3910942aa26564ebfcd817504b73f0
brahamack <l.lawliet.c@qq.com>
2018-10-16 22:58:53 +0800

    first commit

---
+++ a_way.py
@@ -0,0 +1,100 @@
+
+#import MessageBox
+
+def bing_add(cursor,log,A):
+    if A.lineEdit_yaofang.text() == "":
+        try:
+            cursor.execute("INSERT INTO bing (id_bing,name) \
+                VALUES ( '"+str(A.lineEdit_id.text())+"' ,'"+str(A.lineEdit_bingming.text())+"')")
+            log.commit()
+            #massageBox = MessageBox.MessageBox("导入成功")      ?????
+            print("导入病成功")
+        except Exception as e:
+            print(e)
+            print("导入错误")
+
+    elif A.lineEdit_bingming.text() == "":
+        try:
+            cursor.execute("INSERT INTO yaofang (id_bing,yaofang) \
+                VALUES ( '"+str(A.lineEdit_id.text())+"' ,'"+str(A.lineEdit_yaofang.text())+"')")
+            log.commit()
+            #massageBox = MessageBox.MessageBox("导入成功")      ?????
+            print("导入药方成功")
+        except Exception as e:
+            print(e)
+            print("导入错误")
+
+    elif A.lineEdit_1.text() == "":
+        try:
+            cursor.execute("INSERT INTO yaofang (id_bing,yaofang) \
+                VALUES ( '"+str(A.lineEdit_id.text())+"' ,'"+str(A.lineEdit_yaofang.text())+"')")
+            log.commit()
+
+            cursor.execute("INSERT INTO bing (id_bing,name) \
+                            VALUES ( '" + str(A.lineEdit_id.text()) + "' ,'" + str(A.lineEdit_bingming.text()) + "')")
+            log.commit()
+
+            print("导入病和药方成功")
+        except Exception as e:
+            print(e)
+            print("导入错误")
+    else :
+        try:
+            cursor.execute("INSERT INTO yaofang (id_bing,yaofang) \
+                            VALUES ( '" + str(A.lineEdit_id.text()) + "' ,'" + str(A.lineEdit_yaofang.text()) + "')")
+            log.commit()
+
+
+            cursor.execute("INSERT INTO bing (id_bing,name) \
+                                        VALUES ( '" + str(A.lineEdit_id.text()) + "' ,'" + str(
+                A.lineEdit_bingming.text()) + "')")
+            log.commit()
+
+
+            cursor.execute("INSERT INTO yao (id_bing,yao) \
+                VALUES ( '" + str(A.lineEdit_id.text()) + "' ,'" + str(A.lineEdit_1.text()) + "')")
+            log.commit()
+            print("导入病，药方，药成功")
+        except Exception as e:
+            print(e)
+            print("导入错误")
+
+def settable(cursor,window):
+    print("\n")
+
+    try:
+        # cursor.execute("SELECT aid ,type, level, time FROM vio WHERE aid= "+str(id)+" ")
+        cursor.execute("SELECT * FROM bing WHERE aid= '" + str(id) + "' ")
+        print("查询成功")
+        nn = cursor.fetchone()
+        nnn = cursor.fetchall()
+
+        x =len(nn)
+        y =len(nnn)
+        print(len(nn))
+        print(len(nnn))
+        window.tableWidget_yaofang.setColumnCount(x)
+        window.tableWidget_yaofang.setRowCount(y)
+
+        self.tableWidget.setItem(0, 0, QTableWidgetItem("Cell (1,1)"))
+        self.tableWidget.setItem(0, 1, QTableWidgetItem("Cell (1,2)"))
+
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+            # problems
+
+    except Exception as e:
+        print(e)
+
+
+def chaxun(cursor,id):
+    try:
+        cursor.execute("SELECT * FROM bing WHERE name = '"+str(id)+"' ")
+        data = cursor.fetchone()
+        print(data[1])
+            #捕捉输入框的输入进行检索？？？
+        #data[1] == password:
+           # information=self.cursor.fetchall()
+            #return information   #????
+    except Exception as e:
+        print(e)
---
+++ conctrol.py
@@ -0,0 +1,39 @@
+from a_Main import M
+from b_Add import A
+import a_way
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+#数据库开启————————————————————————————————
+import pymysql
+try:
+    log = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='ok120110', db='medical')
+    print("数据库连接成功")
+except Exception as e:
+    print(e)
+    print("数据库连接失败")
+cursor = log.cursor()
+
+#__________________________________
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    M = M()
+    A = A()
+
+#显示——————————————————————
+    M.show()    # 显示log
+
+
+#操作——————————————————————
+   # M.pushButton_bingzheng.clicked.connect(lambda: a_way.chaxun(cursor, mw.lineEdit.text(), Mw2))
+    M.pushButton_out.clicked.connect(lambda: M.hide())
+    A.pushButton_out.clicked.connect(lambda: A.hide())
+    M.pushButton_daoru.clicked.connect(lambda: A.show())
+    A.pushButton_tianjia.clicked.connect(lambda: a_way.bing_add(cursor,log,A))
+    M.pushButton_bingzheng.clicked.connect(lambda: a_way.chaxun(cursor,M.lineEdit_bingzheng.text()))
+
+    sys.exit(app.exec_())
+close
