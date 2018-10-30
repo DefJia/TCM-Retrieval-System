c76abff868569943bd53c1584111cafcc854a8a6
Defjia <code@defjia.top>
2018-10-30 11:29:22 +0800

    Design Frame

--- Front.py
+++ Front.py
@@ -1,5 +1,6 @@
 class Frontend:
     def __init__(self):
         self.type = 2  # 模式
-        self.location = ''
-        self.work_area = dict()
+        self.location = ''  # 当前位置
+        self.search_area = [list(), list(), list(), list()]  # 检索区
+        self.work_area = dict()  # 药方工作区
