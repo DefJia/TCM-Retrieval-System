0f679b3db66108792b7b649cff459eb6bee64cec
贾泽瑞 <defjiachina@gmail.com>
2018-11-01 00:19:31 +0800

    Update Front.py

--- Front.py
+++ Front.py
@@ -6,6 +6,11 @@ class Frontend:
         self.work_area = dict()  # 药方工作区
 
     def get_input(self):
-        # 当获取到输入触发此函数
+        # 当获取到输入触发此函数，然后在下拉框中显示匹配内容
+        pass
+
+    def add_data(self, box_id, content):
+        # 录入模式下，当添加按钮被触发时，将输入的内容添加到数据库
+        # 如果当前位置不为空，还要添加关系
         pass
 
