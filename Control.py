692480272387224a1f6850c6d62d6316d0af46f9
alanlinzy <linziyangdyx@gmail.com>
2018-11-08 10:24:27 +0800

    connect 1st edition

--- Control.py
+++ Control.py
@@ -34,14 +34,67 @@ class Control:
         sys.exit(app.exec_())
 
     def line_text_changed(self):
+        
+         #可能今后会有bug，有点小冲突问题
         self.interface.lineSymptom.textChanged.connect(lambda: self.front.get_input(0))
+        self.interface.lineDisease.textChanged.connect(lambda: self.front.get_input(1))
+        self.interface.linePrescription.textChanged.connect(lambda: self.front.get_input(2))
+        self.interface.lineMedicine.textChanged.connect(lambda: self.front.get_input(3))
+        self.interface.lineBook.textChanged.connect(lambda: self.front.get_input(4))  
+        
+        self.interface.lineSymptom.textChanged.connect(lambda: self.front.set_table(self.interface.symptomOption,self.front.data))#取数据还未完成
+        self.interface.lineDisease.textChanged.connect(lambda:self.front.set_table(self.interface.diseaseOption,self.front.data))
+        self.interface.linePrescription.textChanged.connect(lambda: self.front.set_table(self.interface.prescriptionOption,self.front.data))
+        self.interface.lineMedicine.textChanged.connect(lambda: self.front.set_table(self.interface.medicineOption,self.front.data))
+        #self.interface.lineBook.textChanged.connect(lambda: self.front.set_table(self.interface.bookOption,self.front.data))
+        
+        self.interface.lineSymptom.textChanged.connect(lambda: self.interface.symptomOption.show())
+        self.interface.lineDisease.textChanged.connect(lambda:self.interface.diseaseOption.show())
+        self.interface.linePrescription.textChanged.connect(lambda:  self.interface.prescriptionOption.show())
+        self.interface.lineMedicine.textChanged.connect(lambda: self.interface.medicineOption.show())
+            
+       
+        
         pass
 
     def button_clicked(self):
-        # self.interface.buttonSymptom.clicked.connect(lambda: self.front.())
+        #之后根据front再做变化
+        
+        #if self.interface.lineSymptom.text() != "" :
+        self.interface.buttonSymptom.clicked.connect(lambda: self.front.save_data("symptom",self.interface.lineSymptom.text()))
+        self.interface.buttonSymptom.clicked.connect(lambda: self.front.data.append(self.interface.lineSymptom.text()))#可合并再上个方法
+        self.interface.buttonSymptom.clicked.connect(lambda: self.front.set_table(self.interface.symptomOption,self.front.data))#有错应该再back取数据
+        
+         #if self.interface.lineDisease.text() != "" :
+        self.interface.buttonDisease.clicked.connect(lambda: self.front.save_data("disease",self.interface.lineDisease.text()))
+        self.interface.buttonDisease.clicked.connect(lambda: self.front.data.append(self.interface.lineDisease.text()))
+        self.interface.buttonDisease.clicked.connect(lambda: self.front.set_table(self.interface.diseaseOption,self.front.data))
+        
+         #if self.interface.linePrescription.text() != "" :
+        self.interface.buttonPrescription.clicked.connect(lambda: self.front.save_data("symptom",self.interface.linePrescription.text()))
+        self.interface.buttonPrescription.clicked.connect(lambda: self.front.data.append(self.interface.linePrescription.text()))
+        self.interface.buttonPrescription.clicked.connect(lambda: self.front.set_table(self.interface.prescriptionOption,self.front.data))
+        
+         #if self.interface.lineSymptom.text() != "" :
+        self.interface.buttonMedicine.clicked.connect(lambda: self.front.save_data("symptom",self.interface.lineMedicine.text()))
+        self.interface.buttonMedicine.clicked.connect(lambda: self.front.data.append(self.interface.lineMedicine.text()))
+        self.interface.buttonMedicine.clicked.connect(lambda: self.front.set_table(self.interface.medicineOption,self.front.data))
+        
+            
+        
         pass
 
     def option_clicked(self):
+        self.interface.symptomOption.clicked.connect(lambda:self.interface.lineSymptom.setText(self.interface.symptomOption.selectedItems.text()))
+        self.interface.diseaseOption.clicked.connect(lambda:self.interface.lineDiseasesetText(self.interface.diseaseOption.selectedItems.text()))
+        self.interface.prescriptionOption.clicked.connect(lambda:self.interface.linePrescription.setText(self.interface.prescriptionOption.selectedItems.text()))
+        self.interface.medicineOption.clicked.connect(lambda:self.interface.lineMedicine.setText(self.interface.medicineOption.selectedItems.text()))
+        
+        self.interface.symptomOption.clicked.connect(lambda: self.interface.symptomOption.hide())
+        self.interface.diseaseOption.clicked.connect(lambda: self.interface.diseaseOption.hide())
+        self.interface.prescriptionOption.clicked.connect(lambda: self.interface.prescriptionOption.hide())
+        self.interface.medicineOption.clicked.connect(lambda: self.interface.medicineOption.hide())
+        
         pass
     
 if __name__ == "__main__":
