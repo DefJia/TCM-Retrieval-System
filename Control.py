6d2abd809ba634fd567ce5d6d86466e53c06ad4e
alanlinzy <linziyangdyx@gmail.com>
2019-01-12 22:13:35 +0800

    upgrade

--- Control.py
+++ Control.py
@@ -86,7 +86,8 @@ class Control:
 
     def option_clicked(self, option):
         index = self.group_options.index(option)
-        text = str(option.selectedItems.text())
+        text = str(option.selectedItems()[0].text())
+        print(text)
         self.group_inputs[index].setText(text)
         option.hide()
         option.clicked.connect(lambda: self.front.optioned_data(index, text))
