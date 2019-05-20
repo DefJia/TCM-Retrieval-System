72ccbc2809cc4b064c3e4daa2cfe0505e8e47d3c
贾泽瑞 <defjiachina@gmail.com>
2019-05-21 03:47:19 +0800

    :construction: Work in progress.

--- Controller/Monitor.py
+++ Controller/Monitor.py
@@ -2,44 +2,58 @@ import sys, os
 dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(dir)
 
-from Controller.Method import test
-from Model.test import L
+# from Controller.Method import test
+# from Model.test import L
+from Model.Main import test
 
 
 class Monitor:
     def __init__(self, obj):
-        obj.buttonInput.clicked.connect(lambda:test())
+        self.interface = obj
         # 输入框
         self.group_inputs = list()
-        self.group_inputs.append(obj.lineSymptom)
-        self.group_inputs.append(obj.lineDisease)
-        self.group_inputs.append(obj.linePrescription)
-        self.group_inputs.append(obj.lineMedicine)
+        self.group_inputs.append(self.interface.lineSymptom)
+        self.group_inputs.append(self.interface.lineDisease)
+        self.group_inputs.append(self.interface.linePrescription)
+        self.group_inputs.append(self.interface.lineMedicine)
         # 添加按钮
         self.group_additions = list()
-        self.group_additions.append(obj.buttonSymptom)
-        self.group_additions.append(obj.buttonDisease)
-        self.group_additions.append(obj.buttonPrescription)
-        self.group_additions.append(obj.buttonMedicine)
+        self.group_additions.append(self.interface.buttonSymptom)
+        self.group_additions.append(self.interface.buttonDisease)
+        self.group_additions.append(self.interface.buttonPrescription)
+        self.group_additions.append(self.interface.buttonMedicine)
         # 下拉框
         self.group_options = list()
-        self.group_options.append(obj.symptomOption)
-        self.group_options.append(obj.diseaseOption)
-        self.group_options.append(obj.prescriptionOption)
-        self.group_options.append(obj.medicineOption)
+        self.group_options.append(self.interface.symptomOption)
+        self.group_options.append(self.interface.diseaseOption)
+        self.group_options.append(self.interface.prescriptionOption)
+        self.group_options.append(self.interface.medicineOption)
         # 显示框
         self.group_tables = list()
-        self.group_tables.append(obj.tablewidgetSymptom)
-        self.group_tables.append(obj.tablewidgetDisease)
-        self.group_tables.append(obj.tablewidgetPrescription)
-        self.group_tables.append(obj.tablewidgetMedicine)
-        self.group_tables.append(obj.tablewidgetBook)
-        self.group_tables.append(obj.tablewidgetPrescribe)
+        self.group_tables.append(self.interface.tablewidgetSymptom)
+        self.group_tables.append(self.interface.tablewidgetDisease)
+        self.group_tables.append(self.interface.tablewidgetPrescription)
+        self.group_tables.append(self.interface.tablewidgetMedicine)
+        self.group_tables.append(self.interface.tablewidgetBook)
+        self.group_tables.append(self.interface.tablewidgetPrescribe)
         # init
         self.tables()
 
     def tables(self):
-        n = 0
-        for table in self.group_additions:
-            table.clicked.connect(L(n))
-            n += 1
+        self.interface.tablewidgetSymptom.clicked.connect(lambda: test.table_option_clicked(0))
+        self.interface.tablewidgetDisease.clicked.connect(lambda: test.table_option_clicked(1))
+        self.interface.tablewidgetPrescription.clicked.connect(lambda: test.table_option_clicked(2))
+        self.interface.tablewidgetMedicine.clicked.connect(lambda: test.table_option_clicked(3))
+
+    def options(self):
+        self.interface.symptomOption.clicked.connect(lambda: self.option_clicked(self.interface.symptomOption))
+        self.interface.diseaseOption.clicked.connect(lambda: self.option_clicked(self.interface.diseaseOption))
+        self.interface.prescriptionOption.clicked.connect(lambda: self.option_clicked(self.interface.prescriptionOption))
+        self.interface.medicineOption.clicked.connect(lambda: self.option_clicked(self.interface.medicineOption))
+
+    def inputs(self):
+        self.interface.lineSymptom.textChanged.connect(lambda: self.line_text_changed(self.interface.lineSymptom))
+        self.interface.lineDisease.textChanged.connect(lambda: self.line_text_changed(self.interface.lineDisease))
+        self.interface.linePrescription.textChanged.connect(lambda: self.line_text_changed(self.interface.linePrescription))
+        self.interface.lineMedicine.textChanged.connect(lambda: self.line_text_changed(self.interface.lineMedicine))
+
---
+++ Model/Main.py
@@ -0,0 +1,6 @@
+class test:
+    def __init__(self):
+        pass
+
+    def table_option_clicked(self, a):
+        print(a)
